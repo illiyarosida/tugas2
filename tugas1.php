00f589c371ed11d67b01dea04fad559e02a7218d
unknown <illiyarosida11@gmail.com>
2022-08-29 21:26:02 +0700

    tugas pertemuan 2

---
+++ tugas1.php
@@ -0,0 +1,22 @@
+<?php
+  echo ("<h2>Tipe Data String </h2>");
+  $nama="Illiya rosida";  
+   $nim = "210411100051" ;
+   $kelas = "PAW C" ; 
+   echo "Nama : $nama<br>";
+   echo "Nim  : $nim<br>";
+   echo "Nim  : $kelas<br>";
+?> 
+<?php
+  echo ("<h2>Tipe Data float </h2>");
+  $nilai_f = 15.5;
+  var_dump($nilai_f);
+?>
+<?php
+  echo ("<h2>Tipe Data Bolean </h2>");
+  $coba = true;
+  if ($coba == true)
+{
+    echo 'true';
+}
+?>
